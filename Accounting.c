c6382145e9a0df42dcb25be1842acd41745ea3d6
Ali8231 <aliasiaee304@gmail.com>
2022-01-09 01:27:38 +0330

    accounting.c

--- Accounting.c
+++ Accounting.c
@@ -3,29 +3,59 @@
 #include<string.h>
 #include<windows.h>
 #include<conio.h>
+#include<time.h>
 
+struct UserProfile
+    {
+        char name[30];
+        char family[30];
+        char user_name[30];
+        char password[30];
+        char MelliNum[20];
+        char PhoneNum[20];
+        char Email[50];
+        struct Userprofile *next;
+    };
+
+char username[20];
+
+void SubmitIncome();
+void EntranceMenu();
 void Signup();
-char username[15];
+void Login();
+int UserNameSearch();
+int LoginPassCheck(int password[]);
+void MainMenu();
+int ProfileIteration();
+
+
 void main()
 {
-    char FirstMenuChoice;
     printf("Welcome to Ali accounting software");
     sleep(2);
-    system("cls");
-    printf("1) Signup\n2) Login\n3) Exit\n\nPlease enter a number from the list: ");
-    FirstMenuChoice=getche();
-    if(FirstMenuChoice=='1')
-        Signup();
-
-
+    EntranceMenu();
 }
+
+void EntranceMenu()
+{
+    system("cls");
+    char EntranceMenuChoice;
+    printf("1) Signup\n2) Login\n3) Exit\n\nPlease enter a number from the list: ");
+    EntranceMenuChoice=getche();
+    if(EntranceMenuChoice=='1')
+        Signup();
+    else if(EntranceMenuChoice=='2')
+        Login();
+}
+
 void Signup()
 {
     system("cls");
-    char temp[80],name[20],family[25],password[21],ConfirmPass[21],MelliNum[11],PhoneNum[12],Email[35];
+    char File_Directoy[80],name[20],family[25],password[21],ConfirmPass[21],MelliNum[11],PhoneNum[12],Email[35];
     FILE *profile,*UserIncome,*UserExpense;
     profile=fopen("profile.txt","a");
     printf("-----SIGN UP PAGE-----\n\n\nEnter your first name: ");
+    rewind(profile);
     gets(name);
     strcat(name,"\n");
     fputs(name,profile);
@@ -45,14 +75,14 @@ void Signup()
     //    }
     //}while(UserNameCheck==0);
     printf("\n");
-    strcpy(temp,"F:\\\\C_Programs\\\\Final_Project\\\\incomes\\\\");
-    strcat(temp,username);
-    strcat(temp,".txt");
-    UserIncome=fopen(temp,"w");
-    strcpy(temp,"F:\\\\C_Programs\\\\Final_Project\\\\expenses\\\\");
-    strcat(temp,username);
-    strcat(temp,".txt");
-    UserExpense=fopen(temp,"w");
+    strcpy(File_Directoy,"F:\\\\C_Programs\\\\Final_Project\\\\incomes\\\\");
+    strcat(File_Directoy,username);
+    strcat(File_Directoy,".txt");
+    UserIncome=fopen(File_Directoy,"w");
+    strcpy(File_Directoy,"F:\\\\C_Programs\\\\Final_Project\\\\expenses\\\\");
+    strcat(File_Directoy,username);
+    strcat(File_Directoy,".txt");
+    UserExpense=fopen(File_Directoy,"w");
     strcat(username,"\n");
     fputs(username,profile);
     printf("\n\nNotes about password:\n---Your password must be between 8 and 20 characters\n---Your password must include uppercase and lowercase letters,at least one\n number and one special character(@,*,#,...)\n\nEnter password: ");
@@ -66,8 +96,8 @@ void Signup()
     //        gets(password);
     //    }
     //}while(PassCheck==0);
-     printf("Enter the password again: ");
-     gets(ConfirmPass);
+    printf("Enter the password again: ");
+    gets(ConfirmPass);
     do
     {
        if(strcmp(password,ConfirmPass)!=0)
@@ -119,3 +149,178 @@ void Signup()
     fputs(Email,profile);
     fclose(UserIncome);
 }
+
+
+void Login()
+{
+    int UserNameFound,PassFound;
+    char password[21];
+    system("cls");
+    printf("-----LOGIN PAGE-----\n\n\nEnter your user name: ");
+    gets(username);
+    do
+    {
+        UserNameFound=UserNameSearch();
+        if(UserNameFound==-1)
+        {
+            printf("User name was not found.\nPlease enter your user name again: ");
+            gets(username);
+        }
+    }while(UserNameFound==-1);
+    printf("Enter your password: ");
+    gets(password);
+    do
+    {
+        PassFound=LoginPassCheck(password);
+        if(PassFound==-1)
+        {
+            printf("Password is not correct.\nPlease enter password again: ");
+            gets(password);
+        }
+    }while(PassFound==-1);
+    MainMenu();
+}
+
+
+int UserNameSearch()
+{
+    char temp_Username[25];
+    struct UserProfile *head,*temp;
+    head=(struct UserProfile*)malloc(sizeof(struct UserProfile));
+    head=ProfileIteration();
+    temp=head;
+    strcpy(temp_Username,username);
+    strcat(temp_Username,"\n");
+    while(temp!=NULL)
+    {
+        if(strcmp(temp->user_name,temp_Username)==0)
+            return 0;
+        temp=temp->next;
+    }
+    return -1;
+}
+
+
+int LoginPassCheck(int password[])
+{
+    char temp_Username[25],temp_Password[25];
+    struct UserProfile *head,*temp;
+    head=(struct UserProfile*)malloc(sizeof(struct UserProfile));
+    head=ProfileIteration();
+    temp=head;
+    strcpy(temp_Username,username);
+    strcpy(temp_Password,password);
+    strcat(temp_Username,"\n");
+    strcat(temp_Password,"\n");
+    while(temp!=NULL)
+    {
+        if((strcmp(temp_Username,temp->user_name)==0) && (strcmp(temp_Password,temp->password)==0))
+            return 0;
+        temp=temp->next;
+    }
+    return -1;
+}
+
+
+void MainMenu()
+{
+    system("cls");
+    char MainMenuChoice;
+    printf("-----MAIN MENU-----\n\n\n1) Submit Income\n2) Submit Expenses\n3) Reports\n4) Settings\n5) Logout\n6) Exit\n\nPlease enter a number: ");
+    MainMenuChoice=getche();
+    if(MainMenuChoice=='1')
+        SubmitIncome();
+}
+
+void SubmitIncome()
+{
+    system("cls");
+    char Incomes_Directory[50],IncomeAmount[20],IncomeSource,SpecificSourceOfIncome[30],YearOfIncome[6],MonthOfIncome[4],DayOfIncome[4],IncomeDescription[120];
+    FILE *Incomes;
+    strcpy(Incomes_Directory,"F:\\\\C_Programs\\\\Final_Project\\\\incomes\\\\");
+    strcat(Incomes_Directory,username);
+    strcat(Incomes_Directory,".txt");
+    Incomes=fopen(Incomes_Directory,"a");
+    printf("-----INCOME SUBMIT MENU-----\n\n\nEnter amount of income in Iranian RIAL currency: ");
+    gets(IncomeAmount);
+    strcat(IncomeAmount,"\n");
+    fputs(IncomeAmount,Incomes);
+    printf("\n\n1) Salary\n2) Pocket Money\n3)Government Aid\n4)University Grant\n5) Bank Interest\n6) Loan\n7)Other\n\nPlease choose your source of income: ");
+    IncomeSource=getche();
+    if(IncomeSource=='1')
+        fputs("Salary\n",Incomes);
+    else if(IncomeSource=='2')
+        fputs("Pocket Money\n",Incomes);
+         else if(IncomeSource=='3')
+            fputs("Government Aid\n",Incomes);
+              else if(IncomeSource=='4')
+                fputs("University Grant\n",Incomes);
+                   else if(IncomeSource=='5')
+                    fputs("Bank Interest\n",Incomes);
+                        else if(IncomeSource=='6')
+                            fputs("Loan\n",Incomes);
+                             else
+                             {
+                                 printf("\nPlease enter your source of income: ");
+                                 gets(SpecificSourceOfIncome);
+                                 strcat(SpecificSourceOfIncome,"\n");
+                                 fputs(SpecificSourceOfIncome,Incomes);
+                             }
+    printf("Please enter year of income in YYYY format: ");
+    gets(YearOfIncome);
+    strcat(YearOfIncome,"//");
+    fputs(YearOfIncome,Incomes);
+    printf("Please enter month of income in MM format: ");
+    gets(MonthOfIncome);
+    strcat(MonthOfIncome,"//");
+    fputs(MonthOfIncome,Incomes);
+    printf("Please enter day of income in DD format: ");
+    gets(DayOfIncome);
+    strcat(DayOfIncome,"\n");
+    fputs(DayOfIncome,Incomes);
+    printf("Please enter a short(one line) description: ");
+    gets(IncomeDescription);
+    strcat(IncomeDescription,"\n");
+    fputs(IncomeDescription,Incomes);
+}
+
+
+ int ProfileIteration()
+{
+    struct UserProfile *head,*user,*temp;
+    FILE *profile;
+    profile=fopen("profile.txt","r");
+    do
+    {
+        head=(struct UserProfile*)malloc(sizeof(struct UserProfile));
+    }while(head==NULL);
+    fgets(head->name,sizeof head->name,profile);
+    fgets(head->family,sizeof head->family,profile);
+    fgets(head->user_name,sizeof head->user_name,profile);
+    fgets(head->password,sizeof head->password,profile);
+    fgets(head->MelliNum,sizeof head->MelliNum,profile);
+    fgets(head->PhoneNum,sizeof head->PhoneNum ,profile);
+    fgets(head->Email,sizeof head->Email,profile);
+    head->next=NULL;
+    temp=head;
+    while(feof(profile)==0)
+    {
+        do
+        {
+            user=(struct UserProfile*)malloc(sizeof(struct UserProfile));
+        }while(user==NULL);
+        fgets(user->name,sizeof user->name,profile);
+        fgets(user->family,sizeof user->family,profile);
+        fgets(user->user_name,sizeof user->user_name,profile);
+        fgets(user->password,sizeof user->password,profile);
+        fgets(user->MelliNum,sizeof head->MelliNum,profile);
+        fgets(user->PhoneNum,sizeof user->PhoneNum,profile);
+        fgets(user->Email,sizeof head->Email,profile);
+        user->next=NULL;
+        temp->next=user;
+        temp=user;
+    }
+    fclose(profile);
+    return head;
+}
+
